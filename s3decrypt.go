c6293e52329041db4f94503e406c6255a412bd0a
DonMills <dmills@singlestoneconsulting.com>
2016-09-22 19:18:16 -0400

    Cleaning comments

--- s3decrypt.go
+++ s3decrypt.go
@@ -51,16 +51,12 @@ func fetchkey(remfilename string, bucket string, context string) []byte {
 	plainkey, err := service.Decrypt(keyparams)
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
-			// Generic AWS error with Code, Message, and original error (if any)
 			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
 			if reqErr, ok := err.(awserr.RequestFailure); ok {
-				// A service error occurred
 				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
 				os.Exit(1)
 			}
 		} else {
-			// This case should never be hit, the SDK should always return an
-			// error which satisfies the awserr.Error interface.
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
@@ -81,24 +77,18 @@ func fetchfile(remfilename string, bucket string) ([]byte, []byte, []byte) {
 
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
-			// Generic AWS error with Code, Message, and original error (if any)
 			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
 			if reqErr, ok := err.(awserr.RequestFailure); ok {
-				// A service error occurred
 				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
 				os.Exit(1)
 			}
 		} else {
-			// This case should never be hit, the SDK should always return an
-			// error which satisfies the awserr.Error interface.
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
 	}
 
-	//decode := base64.NewDecoder(base64.StdEncoding,file.Body)
 	data, _ := ioutil.ReadAll(file.Body)
-	//data := alldata[aes.BlockSize:]
 	encodeiv := *file.Metadata["X-Amz-Iv"]
 	s3encodekey := *file.Metadata["X-Amz-Key"]
 	iv, _ := base64.StdEncoding.DecodeString(encodeiv)
